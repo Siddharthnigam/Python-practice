42951d07a4b410e9fdd87aba46b6172680db97d4
Siddharth Nigam <siddharthjinigam@gmail.com>
2025-02-02 21:53:29 +0530

    change

---
+++ otp.py
@@ -0,0 +1,15 @@
+# def function(length):
+#     otp=""   
+#     for i in range(1,length):
+#       otp+=Math.floor(Math.random()*10)
+#       print(i)
+
+import random
+
+def generate_otp(length):
+    otp = ""   
+    for i in range(length):
+        otp += str(random.randint(0, 9))
+    print(otp)
+    
+generate_otp(6)  # Example usage to generate a 6-digit OTP
